b571ca902356dd0072798d5d22da318f6d717cb9
Rahul kumar jha <jha746348@gmail.com>
2024-09-13 00:58:25 +0530

    loop

---
+++ Pattern_1.java
@@ -0,0 +1,13 @@
+import java.net.SocketOption;
+import java.sql.SQLOutput;
+
+public class Pattern_1
+{
+    public static void main (String args[])
+    {
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("*");
+        }
+    }
+}
